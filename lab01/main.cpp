b370279ff04c31046a5500acee92b485cd210fee
raymondborkowski <rborkows@umich.edu>
2017-12-13 17:43:01 -0500

    RB: Lab01 280

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
+#include <cstdlib>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void printGreeting(int population) {
+    if (population == 0) {
+        cout << "And then there were none..." << endl;
+    }
+    else if (population < 100) {
+        cout << "It's a small world after all!" << endl;
+    }
+    else {
+        cout << "It's a big world out there!" << endl;
+    }
+    
+    cout << "Goodbye World!" << endl;
+}
+
+int main() {
+    cout << "Hello World!" << endl;
+    cout << "How many people are there?" << endl;
+    int pop;
+    cin >> pop;
+    printGreeting(pop);
+    
     return 0;
 }
+

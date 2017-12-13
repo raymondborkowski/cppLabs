6bc9611039268a6526be66be9aa57005596bbb49
raymondborkowski <rborkows@umich.edu>
2017-12-13 16:57:08 -0500

    Initial Commit

---
+++ lab01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab01
+//
+//  Created by Raymond Borkowski on 12/13/17.
+//  Copyright © 2017 Raymond Borkowski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
